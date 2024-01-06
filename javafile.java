db44ea9d14dda0991f63867fc5224a0c1dbc9307
root <ahmed.sufyan1310@gmail.com>
2024-01-06 15:29:40 +0000

    shell and java file from linux container

---
+++ javafile.java
@@ -0,0 +1,5 @@
+public class javafile {
+        public static void main(String[] args) {
+                System.out.println("Java file in a linux container");
+        }
+}
---
+++ shellscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "This is being run on linux container"
+whoami
